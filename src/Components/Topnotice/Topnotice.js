edcbe135124e1f73a7213d30956684d2d2916515
webdeveloperbashar <webdeveloperbashar>
2021-04-06 18:09:19 +0600

    marquee updated

--- src/Components/Topnotice/Topnotice.js
+++ src/Components/Topnotice/Topnotice.js
@@ -1,12 +1,20 @@
 /* eslint-disable jsx-a11y/no-distracting-elements */
 import React from "react";
-import './Topnotice.css';
+import "./Topnotice.css";
 const Topnotice = () => {
   return (
     <div className="welcome-text">
-    <marquee>
-     <strong>❤️Welcome To BetBDT.com ⚽ Good News 09/04/2021 INDIAN Premier League (IPL) উপলক্ষে অল ইউজার পাবেন 10% করে ডিপোজিট বোনাস Club Holder পাবেন 10% করে Income বোনাস FOR Indian Premier League (IPL) ANY Problem Knock Your Club Holder ..Thank You❤️(Trensfers Kora Nisad)❤️</strong>
-    </marquee>
+      <div className="container-fluid">
+        <marquee>
+          <strong>
+            ❤️Welcome To BetBDT.com ⚽ Good News 09/04/2021 INDIAN Premier
+            League (IPL) উপলক্ষে অল ইউজার পাবেন 10% করে ডিপোজিট বোনাস Club
+            Holder পাবেন 10% করে Income বোনাস FOR Indian Premier League (IPL)
+            ANY Problem Knock Your Club Holder ..Thank You❤️(Trensfers Kora
+            Nisad)❤️
+          </strong>
+        </marquee>
+      </div>
     </div>
   );
 };
