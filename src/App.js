04e15312dfed0813c38e10e314cdd94c5c256cb9
hasanmiaweb <hasanmiaweb@gmail.com>
2021-04-10 15:32:08 +0600

    hasan

--- src/App.js
+++ src/App.js
@@ -1,8 +1,5 @@
 import "bootstrap/dist/css/bootstrap.min.css";
-import {
-  BrowserRouter as Router,
-  Route, Switch
-} from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import "./App.css";
 import Footer from "./Components/Footer/Footer";
 import Header from "./Components/Header/Header";
@@ -13,31 +10,32 @@ import SignUp from "./Components/Pages/SignUp/SignUp";
 import DepositRequest from "./Components/UserProfile/DepositRequest";
 import MyProfile from "./Components/UserProfile/MyProfile";
 function App() {
-  return <Router>
-    <Header></Header>
-    <Switch>
-      <Route exact path="/">
-        <Home />
-      </Route>
-      <Route path="/login">
-        <Login />
-      </Route>
-      <Route path="/signup">
-        <SignUp />
-      </Route>
-      <Route path="/myprofile">
-        <MyProfile />
-      </Route>
-      <Route path="/Deposit">
-        <DepositRequest />
-      </Route>
-      <Route path="*">
-        <Error />
-      </Route>
-    </Switch>
-    <Footer></Footer>
-  </Router>
-
+  return (
+    <Router>
+      <Header></Header>
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route path="/login">
+          <Login />
+        </Route>
+        <Route path="/signup">
+          <SignUp />
+        </Route>
+        <Route path="/myprofile">
+          <MyProfile />
+        </Route>
+        <Route path="/Deposit">
+          <DepositRequest />
+        </Route>
+        <Route path="*">
+          <Error />
+        </Route>
+      </Switch>
+      <Footer></Footer>
+    </Router>
+  );
 }
 
 export default App;
