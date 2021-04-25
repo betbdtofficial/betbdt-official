076561bfc80f510e5e03637a396f3784f1ebf38f
hasanmiaweb <hasanmiaweb@gmail.com>
2021-04-25 11:30:52 +0600

    hasan

--- src/Components/Pages/Slider/Accordion/FballLiveAccordion.js
+++ src/Components/Pages/Slider/Accordion/FballLiveAccordion.js
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from "react";
 import { Accordion, Card } from "react-bootstrap";
 import football from "../../../image/SliderImg/f_ball.png";
 import "../Slider.css";
+import PlaceBetFrom from "./PlaceBetFrom";
 
 function FballLiveAccordion() {
   const liveMatch = [1255828,1260256,1254075]
@@ -23,11 +24,18 @@ function FballLiveAccordion() {
             <Accordion.Collapse eventKey="0">
               <div className="my-3 text-center">
                 <div className="team1">
+                <div className="team1Themb">
+                  <img src="https://i.ibb.co/3v4T51g/800px-Flag-of-Bangladesh-svg.png" width="150px" alt=""/>
+                </div>
                   <strong>Bangladesh <span>1.9</span></strong>
                 </div> <h4>VS</h4> <div className="team2">
-                  <strong>India <span>1.5</span></strong>
+                <div className="team1Themb">
+                  <img src="https://i.ibb.co/QvJFdsj/1200px-Flag-of-India-svg.png" width="150px" alt=""/>
                 </div>
-                <button>Place bet</button>
+                  <strong>India <span>1.5</span></strong>
+                </div> <br/>
+                {/* <button>Place bet</button> */}
+                <PlaceBetFrom></PlaceBetFrom>
                 <br/>
               </div>
             </Accordion.Collapse>
---
+++ src/Components/Pages/Slider/Accordion/PlaceBetFrom.js
@@ -0,0 +1,56 @@
+import React from "react";
+import Modal from "react-modal";
+import './PlaceBetFrom.css';
+const customStyles = {
+  content: {
+    top: "50%",
+    left: "50%",
+    right: "auto",
+    bottom: "auto",
+    marginRight: "-50%",
+    transform: "translate(-50%, -50%)",
+  },
+};
+Modal.setAppElement("#root");
+const PlaceBetFrom = () => {
+  var subtitle;
+  const [modalIsOpen, setIsOpen] = React.useState(false);
+  function openModal() {
+    setIsOpen(true);
+  }
+
+  function afterOpenModal() {
+    // references are now sync'd and can be accessed.
+    subtitle.style.color = "#f00";
+  }
+
+  function closeModal() {
+    setIsOpen(false);
+  }
+  return (
+    <div>
+      <button onClick={openModal}>Place bet</button>
+      <Modal
+        isOpen={modalIsOpen}
+        onAfterOpen={afterOpenModal}
+        onRequestClose={closeModal}
+        style={customStyles}
+        contentLabel="Example Modal"
+      >
+        <h2 className="text-center" ref={(_subtitle) => (subtitle = _subtitle)}>Place Bet</h2>
+        <button onClick={closeModal}>close</button>
+        <div>I am a modal
+        <form>
+          <input />
+          <button>tab navigation</button>
+          <button>stays</button>
+          <button>inside</button>
+          <button>the modal</button>
+        </form>
+        </div>
+      </Modal>
+    </div>
+  );
+};
+
+export default PlaceBetFrom;
