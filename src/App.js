c19489cfb67ac38cc008c21a25c3d0f20b2aaa38
hasanmiaweb <hasanmiaweb@gmail.com>
2021-05-08 22:01:47 +0600

    hasan

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import "./App.css";
 import ClubHolder from "./Components/ClubHolder/ClubHolder";
 import Dashboard from "./Components/Dashboard/Dashboard";
 import FrontEnd from "./Components/Pages/Front-end/FrontEnd";
+import PrivateRoute from "./Components/Pages/PrivateRoute/PrivateRoute";
 export const Context = createContext();
 function App() {
   const [loginUser, setLoginUser] = useState([]);
@@ -12,12 +13,12 @@ function App() {
     <Context.Provider value={[loginUser, setLoginUser]}>
       <Router>
         <Switch>
-          <Route path="/admin">
+          <PrivateRoute path="/admin">
             <Dashboard />
-          </Route>
-          <Route path="/clubholderDeshboard">
+          </PrivateRoute>
+          <PrivateRoute path="/clubholderDeshboard">
             <ClubHolder />
-          </Route>
+          </PrivateRoute>
           <Route path="/">
             <FrontEnd />
           </Route>
